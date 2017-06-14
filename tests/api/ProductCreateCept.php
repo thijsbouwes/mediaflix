e470a7424710de8954d57480eac2de15a69a59b4
Thijs Bouwes <thijsbouwes@gmail.com>
2017-06-14 21:58:39 +1200

    Added a product delete test

--- tests/api/ProductCreateCept.php
+++ tests/api/ProductCreateCept.php
@@ -12,13 +12,13 @@ $I->canSeeResponseIsJson();
 $I->seeResponseContainsJson(['name' => 'Example product']);
 $I->seeResponseContainsJson(['cost' => '100.55']);
 $I->seeResponseContainsJson(['id' => '1']);
-$I->seeResponseCodeIs(201);
+$I->seeResponseCodeIs(\Codeception\Util\HttpCode::CREATED);
 
 $I->sendGET('/products/1');
 $I->canSeeResponseIsJson();
 $I->seeResponseContainsJson(['name' => 'Example product']);
 $I->seeResponseContainsJson(['cost' => '100.55']);
 $I->seeResponseContainsJson(['id' => '1']);
-$I->seeResponseCodeIs(200);
+$I->seeResponseCodeIs(\Codeception\Util\HttpCode::OK);
 
 $I->seeRecord('products', ['name' => 'Example product', 'cost' => '100.55', 'id' => '1']);
