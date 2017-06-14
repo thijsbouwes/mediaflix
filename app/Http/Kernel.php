18710a955ee8853f2836ca30c9b0fe5801562b3e
Thijs Bouwes <thijsbouwes@gmail.com>
2017-06-14 19:25:31 +1200

    Registred the Json middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\JsonApi;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -38,7 +39,8 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
-            'bindings',
+            JsonApi::class,
+            'bindings'
         ],
     ];
 
