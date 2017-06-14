bf037bd8599533cd48fcd014e03f8a843954ab2b
Thijs Bouwes <thijsbouwes@gmail.com>
2017-06-14 19:25:16 +1200

    Added the fillable fields for the product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'cost'
+    ];
+
+    /**
      * Get the expenses for the product.
      */
     public function expenses()
