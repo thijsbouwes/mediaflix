2f34486c0be102619bd71b1e558eefe10e08a9e5
Thijs Bouwes <thijsbouwes@gmail.com>
2017-06-15 21:13:39 +1200

    Updated the cest test with most of the crud operations

--- tests/api/ProductResourceCest.php
+++ tests/api/ProductResourceCest.php
@@ -5,11 +5,12 @@ use Codeception\Util\HttpCode;
 class ProductResourceCest
 {
     protected const ENDPOINT = '/products';
+    protected const MODEL = 'products';
 
     public function getAllProducts(ApiTester $I)
     {
-        $productOneId = $this->haveProduct($I, ['name' => 'xbox', 'cost' => '100.55']);
-        $productTwoId = $this->haveProduct($I, ['name' => 'playstation', 'cost' => '222.22']);
+        $productOneId = $I->haveRecord(self::MODEL, ['name' => 'xbox', 'cost' => '100.55']);
+        $productTwoId = $I->haveRecord(self::MODEL, ['name' => 'playstation', 'cost' => '222.22']);
 
         $I->sendGET(self::ENDPOINT);
         $I->seeResponseCodeIs(HttpCode::OK);
@@ -25,26 +26,47 @@ class ProductResourceCest
 
     public function getSingleProduct(ApiTester $I)
     {
+        $productOneId = $I->haveRecord(self::MODEL, ['name' => 'xbox', 'cost' => '100.55']);
+        $I->sendGET(self::ENDPOINT."/{$productOneId}");
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->canSeeResponseIsJson();
 
+        $I->expect('item is present in response');
+        $I->seeResponseContainsJson(['id' => $productOneId, 'name' => 'xbox', 'cost' => '100.55']);
     }
 
     public function createProduct(ApiTester $I)
     {
+        $I->sendPOST(self::ENDPOINT, json_encode([
+            'name' => 'xbox',
+            'cost' => '100.55'
+        ]));
+        $I->seeResponseCodeIs(HttpCode::CREATED);
+        $I->seeResponseContainsJson();
 
+        $I->expect('item is created and is available in response');
+        $I->seeResponseContainsJson(['id' => 1, 'name' => 'xbox', 'cost' => '100.55']);
     }
 
     public function updateProduct(ApiTester $I)
     {
+        $productOneId = $I->haveRecord(self::MODEL, ['name' => 'xbox', 'cost' => '100.55']);
+        $I->sendPATCH(self::ENDPOINT."/{$productOneId}", json_encode([
+            'name' => 'playstation',
+            'cost' => '222.22'
+        ]));
+        $I->seeResponseCodeIs(HttpCode::NO_CONTENT);
 
+        $I->expect('item is updated');
+        $I->seeRecord(self::MODEL, ['id' => 1, 'name' => 'playstation', 'cost' => '222.22']);
     }
 
-    private function haveProduct(ApiTester $I, array $data = [])
+    public function deleteProduct(ApiTester $I)
     {
-        $data = array_merge([
-            'name' => 'Example product',
-            'cost' => '100.55'
-        ], $data);
+        $productOneId = $I->haveRecord(self::MODEL, ['name' => 'xbox', 'cost' => '100.55']);
+        $I->sendDELETE(self::ENDPOINT."/{$productOneId}");
 
-        return $I->haveRecord('products', $data);
+        $I->seeResponseCodeIs(HttpCode::NO_CONTENT);
+        $I->dontSeeRecord(self::MODEL, ['name' => 'xbox', 'cost' => '100.55']);
     }
 }
