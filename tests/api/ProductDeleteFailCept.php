ff2474dba331408c08ad4d5fe091b4c8f9ac42e7
Thijs Bouwes <thijsbouwes@gmail.com>
2017-06-15 21:28:24 +1200

    Deleted the cept tests because we are using cest testing

--- tests/api/ProductDeleteFailCept.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$I = new ApiTester($scenario);
-$I->wantTo('fail deleting a non-existing product');
-
-$I->sendDELETE('/products/100101');
-$I->canSeeResponseIsJson();
-$I->seeResponseContainsJson(['message' => 'Record not found']);
-$I->seeResponseCodeIs(\Codeception\Util\HttpCode::NOT_FOUND);
