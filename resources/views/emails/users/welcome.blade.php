1ff549d28c9001e0983ac5840b7c9d1df030906f
Thijs Bouwes <thijsbouwes@gmail.com>
2017-11-03 16:02:37 +0100

    Added a nice gif to the welcome email

--- resources/views/emails/users/welcome.blade.php
+++ resources/views/emails/users/welcome.blade.php
@@ -3,6 +3,8 @@
 
 Welcome to {{ config('app.name') }} your account has been created!
 
+![Welcome image](https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif "Logo Title Text 1")
+
 @component('mail::button', ['url' => config('app.url').'admin'])
     View Account
 @endcomponent
