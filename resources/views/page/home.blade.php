059bd90080dc57203d03d3624afa238fa106906e
Thijs Bouwes <thijsbouwes@gmail.com>
2017-11-04 10:48:56 +0100

    Updated the home page and images

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -8,12 +8,13 @@
         <div class="row">
             <div class="col s12 center-align white-text">
                 <h1 class="valign">
-                    <span class="bold">{{ config('app.name', 'Skihut') }}</span>
-                    <span class="thin"><br>The social connection</span>
+                    <span class="bold">{{ config('app.name', 'Mediaflix') }}</span>
+                    <span class="thin"><br>The best media streamer</span>
                 </h1>
 
                 @auth
-                    <a class="waves-effect waves-light btn-large" href="{{ route('dashboard') }}">Dashboard<i class="material-icons right">account_circle</i></a>
+                    <a class="waves-effect waves-light btn-large" href="{{ route('dashboard') }}">View media<i class="material-icons right">play_circle_filled
+                            play_circle_filled</i></a>
                 @endauth
 
                 @guest
@@ -30,7 +31,7 @@
                 <div class="col s4">
                     <div class="center promo promo-example">
                         <i class="material-icons">flash_on</i>
-                        <p class="promo-caption">Versnelt ontwikkeling</p>
+                        <p class="promo-caption">Super snel</p>
                         <p class="light center">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components.</p>
                     </div>
                 </div>
@@ -54,7 +55,7 @@
 
     <section>
         <div class="parallax-container">
-            <div class="parallax"><img src="/storage/images/code.jpg"></div>
+            <div class="parallax"><img src="/storage/images/movies.jpg"></div>
         </div>
     </section>
 
@@ -63,16 +64,16 @@
             <h1 class="center-align">FAQ</h1>
             <ul class="collapsible popout" data-collapsible="accordion">
                 <li>
-                    <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                    <div class="collapsible-header"><i class="material-icons">devices</i>Hoeveel apparaten?</div>
+                    <div class="collapsible-body"><span>Per account kunnen er 3 apparaten worden gebruikt.</span></div>
                 </li>
                 <li class="active">
-                    <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                    <div class="collapsible-header"><i class="material-icons">language</i>In wat voor taal is de media?</div>
+                    <div class="collapsible-body"><span>De media wordt in verschillende talen aangeboden</span></div>
                 </li>
                 <li>
-                    <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-                    <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+                    <div class="collapsible-header"><i class="material-icons">whatshot</i>New content?</div>
+                    <div class="collapsible-body"><span>Wij proberen zoveel mogelijk content up to date te houden.</span></div>
                 </li>
             </ul>
         </div>
